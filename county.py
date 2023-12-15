a669ef0e1998e4bc162b3c3589e7f69aa35efe46
SuperZekes <149198602+SuperZekes@users.noreply.github.com>
2023-12-15 11:17:41 -0400

    Update county.py

--- county.py
+++ county.py
@@ -1,22 +1,7 @@
 import time
 
-print("1")
-time.sleep(1)
-print("2")
-time.sleep(1)
-print("3")
-time.sleep(1)
-print("4")
-time.sleep(1)
-print("5")
-time.sleep(1)
-print("6")
-time.sleep(1)
-print("7")
-time.sleep(1)
-print("8")
-time.sleep(1)
-print("9")
-time.sleep(1)
-print("10")
-time.sleep(1)
+x = 0
+while True:
+   x = x+1
+   time.sleep(1)
+   print (f'{x}')
